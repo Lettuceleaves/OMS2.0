aafb0eddc16d68b014b29397e675b86c914ca499
LettuceLeaves <2878506229@qq.com>
2025-04-25 01:30:09 +0800

    "新建文件夹"

---
+++ docManage/src/test/java/org/example/docmanage/DocManageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.docmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ infoTable/src/main/java/org/example/infotable/InfoTableApplication.java
@@ -0,0 +1,13 @@
+package org.example.infotable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoTableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfoTableApplication.class, args);
+    }
+
+}
